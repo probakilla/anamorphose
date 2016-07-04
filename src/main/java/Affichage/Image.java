cfae97cd71dbc94f2032fccd7dac81c9a29a4570
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2016-07-04 11:48:09 +0200

    Ajout d'une classe image et changement de la fonction de chargement. Celle-ci ouvre désormais une bufferedimage

---
+++ src/main/java/Affichage/Image.java
@@ -0,0 +1,16 @@
+package Affichage;
+
+import java.awt.image.BufferedImage;
+
+public class Image {
+
+    public BufferedImage _image;
+    
+    public Image () {
+        _image = null;
+    }
+    
+    public void setImage (BufferedImage image) {
+        _image = image;
+    }
+}
--- src/main/java/Boutons/ChargerImage.java
+++ src/main/java/Boutons/ChargerImage.java
@@ -1,7 +1,10 @@
 package Boutons;
 
+import Affichage.Fenetre;
+import Affichage.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -10,33 +13,31 @@ import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 
 public class ChargerImage implements ActionListener {
-    JFrame nomFenetre;
     
-    // Récupère le nom de la fenetre où l'on veut afficher l'image.
-    public ChargerImage (JFrame f) {
-        nomFenetre = f;
+    Image _image;
+
+    public ChargerImage (Image image) {
+    
+        _image = image;
     }
     
     public void actionPerformed(ActionEvent e) {
         
-        // Récupère l'image
+        // Rï¿½cupï¿½re l'image
         MBFImage source = null;
         JFileChooser fichier = new JFileChooser();
         fichier.showOpenDialog(null);
         
-        // Verrifie si le fichier sélectionné est une image.
+        // Verrifie si le fichier sï¿½lectionnï¿½ est une image.
         try {
             source = ImageUtilities.readMBF(fichier.getSelectedFile());
         } catch (IOException ex) {
             System.err.printf("Veuillez sÃ©lectionner une image.");
         }
         
-        if (source.getHeight() > nomFenetre.getHeight() || source.getWidth() > nomFenetre.getWidth()) {
-            nomFenetre.setSize(source.getWidth(), source.getHeight());
-            
-        }
-                
+        _image.setImage(ImageUtilities.createBufferedImage(source));
+        
         // Affiche l'image choisie.
-        DisplayUtilities.display(source, nomFenetre);
+        DisplayUtilities.display(source);
     }
 }
