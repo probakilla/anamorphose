24469c864372d08eb3673a4b2e844a83c6e674b8
Probak <Probak@192.168.1.18>
2016-07-25 15:18:52 +0200

    Creation de la classe anamorphose cylindirque

--- src/main/java/Actions/AnamorphoseCylindrique.java
+++ src/main/java/Actions/AnamorphoseCylindrique.java
@@ -18,6 +18,16 @@ public class AnamorphoseCylindrique implements ActionListener
         _image = image;
     }
     
+    private int nouveauX (double angle, int x, int y, int xc, int yc)
+    {
+        return (int) (((Math.cos(angle) * (x - xc)) - (Math.sin(angle) * (y - yc))) + xc);
+    }
+    
+    private int nouveauY (double angle, int x, int y, int xc, int yc)
+    {
+        return (int) (((Math.sin(angle) * (x - xc)) - (Math.cos(angle) * (y - yc))) + yc);
+    }
+    
     public void actionPerformed(ActionEvent e)
     {
         // Recupere l'image.
@@ -46,7 +56,7 @@ public class AnamorphoseCylindrique implements ActionListener
         * On trace un cercle grace a l'algorithme de Bersenham.
         * Cet algorithme trace un octant de cercle,
         * pour tracer un cercle complet (ou ici un demi cercle),
-        * il nous suffit juste de tracer les autres octants grace a une symetrie. 
+        * il nous suffit juste de tracer les autres octants grace a une symetrie.
         * Pour faire correspondre l'image au cercle dessine nous divisions notre
         * image en quatre ce qui nous donne quatre variables (quart1, quart2 etc...)
         * qui nous servent a placer les pixels de chaque quart de l'image.
@@ -54,88 +64,128 @@ public class AnamorphoseCylindrique implements ActionListener
         
         // Variable temporaire.
         int RGBtmp;
+        double angle = 180 / (largeur - 1);
         int centreImage = largeur / 2;
         int centre = tableau3D.getWidth() / 2;
         
         // On place d'abord placer les premiers points de chaque octant de cercle.
-        for (int y = 0; y < hauteur; y ++) {
-            int x, z, d, quart1, quart2, quart3, quart4;
-            double a, b;
-            x = 0;
-            z = rayon;
-            d = 1 - rayon;
-            quart1 = 0;
-            quart2 = centreImage;
-            quart3 = centreImage + 1;
-            quart4 = largeur - 1;
-            // Place le premier point du premier quart de l'image.
-            RGBtmp = anamorphose.getRGB(quart1++, y);
-            tableau3D.setPixel(centre - rayon, y, 0, RGBtmp);
-            tableau3D.setPixel((centre - rayon) - y, 0, 0, RGBtmp);
-            // Place le premier point du deuxieme quart de l'image.
-            RGBtmp = anamorphose.getRGB(quart2--, y);
-            tableau3D.setPixel(centre, y, rayon, RGBtmp);
-            tableau3D.setPixel(centre, 0, y + rayon, RGBtmp);
-            // Place le premier point du troisieme quart de l'image.
-            RGBtmp = anamorphose.getRGB(quart3++, y);
-            tableau3D.setPixel(centre + 1, y, rayon, RGBtmp);
-            tableau3D.setPixel(centre + 1, 0, y + rayon, RGBtmp);
-            // Place le premier point du quatrieme quart de l'image.
-            RGBtmp = anamorphose.getRGB(quart4--, y);
-            tableau3D.setPixel(centre + rayon, y, 0, RGBtmp);
-            tableau3D.setPixel(centre + rayon + y, 0, 0, RGBtmp);
-            
-            // On dessine ensuite le reste des onctants.
-            while (z > x) {
-                if (d < 0) {
-                    d += 2 * x + 3;
-                }
-                else {
-                    d += 2 * (x - z) + 5;
-                    z --;
-                }
-                x ++;
-                
-                
-                int zTmp;
-                // Place le pixel du premier quart.
-                RGBtmp = anamorphose.getRGB(quart1++, y);
-                tableau3D.setPixel(centre - z, y, x, RGBtmp);
-                a = (double) ((0 - x) / (centre - (centre - z)));
-                b = x - (a * (centre - z));
-                zTmp = (int) (a * (centre + x) + b);
-                //tableau3D.setPixel(centre - z, 0, y + zTmp, RGBtmp);
-                
-                // Place le pixel du deuxieme quart.
-                RGBtmp = anamorphose.getRGB(quart2--, y);
-                tableau3D.setPixel(centre - x, y, z, RGBtmp);
-                a = (double) ((0 - z) / (centre - (centre - x)));
-                b = z - (a * (centre - x));
-                zTmp = (int) (a * (centre + x) + b);
-                //tableau3D.setPixel(centre - x, 0, zTmp + rayon, RGBtmp);
-                
-                // Place le pixel du troisieme quart.
-                RGBtmp = anamorphose.getRGB(quart3++, y);
-                tableau3D.setPixel(centre + x, y, z, RGBtmp);
-                a = (double) ((z - 0) / ((centre + x) - centre));
-                b = 0 - (a * centre);
-                zTmp = (int) (a * (centre + x) + b);
-                tableau3D.setPixel(centre + x, 0, zTmp + y, RGBtmp);
-                
-                // Place le pixel du quatrieme quart.
-                RGBtmp = anamorphose.getRGB(quart4--, y);
-                tableau3D.setPixel(centre + z, y, x, RGBtmp);
-                a = (double) ((x - 0) / ((centre + z) - centre));
-                b = 0 - (a * (centre));
-                zTmp = (int) (a * (centre + x) + b);
-                tableau3D.setPixel(centre + z, 0, zTmp + x, RGBtmp);
-            }
+        //for (int y = 0; y < hauteur; y ++) {
+        int x, z, d, quart1, quart2, quart3, quart4;
+        double a, b;
+        // Variables utilisees pour garder en memoire la valeur des coordonnes des points de l'ancien tour de boucle.
+        int x1Pre, y1Pre, x2Pre, y2Pre, x3Pre, y3Pre, x4Pre, y4Pre;
+        
+        x = 0;
+        z = rayon;
+        d = 1 - rayon;
+        quart1 = 0;
+        quart2 = centreImage;
+        quart3 = centreImage + 1;
+        quart4 = largeur - 1;
+        // Place le premier point du premier quart de l'image.
+        RGBtmp = anamorphose.getRGB(quart1, 0);
+        tableau3D.setPixel(centre - rayon, 0, 0, RGBtmp);
+        // Trace la première ligne droite.
+        for (int xTmp = (centre - rayon); xTmp >= ((centre - rayon) - hauteur); xTmp --) {
+            int tmp = 1; // Variable necessaire pour la couleur d'une colonne.
+            RGBtmp = anamorphose.getRGB(quart1, tmp++);
+            tableau3D.setPixel(xTmp, 0, 0, RGBtmp);
         }
+        x1Pre = ((centre - rayon) - hauteur);
+        y1Pre = 0;
+        quart1++;
+        
+        // Place le premier point du deuxieme quart de l'image.
+        RGBtmp = anamorphose.getRGB(quart2, 0);
+        tableau3D.setPixel(centre, 0, rayon, RGBtmp);
+        // Trace la deuxieme ligne droite.
+        for (int yTmp = rayon; yTmp < (rayon + hauteur); yTmp ++) {
+            int tmp = 1;
+            RGBtmp = anamorphose.getRGB(quart2, tmp++);
+            tableau3D.setPixel(centre, 0, yTmp, RGBtmp);
+        }
+        x2Pre = centre;
+        y2Pre = (rayon + hauteur);
+        quart2--;
+        
+        // Place le premier point du troisieme quart de l'image.
+        RGBtmp = anamorphose.getRGB(quart3, 0);
+        tableau3D.setPixel(centre + 1, 0, rayon, RGBtmp);
+        // Trace la troiseme ligne droite
+        for (int yTmp = rayon; yTmp < (rayon + hauteur); yTmp ++) {
+            int tmp = 1;
+            RGBtmp = anamorphose.getRGB(quart3, tmp++);
+            tableau3D.setPixel(centre + 1, 0, yTmp, RGBtmp);
+        }
+        x3Pre = centre + 1;
+        y3Pre = (rayon + hauteur);
+        quart3++;
+        
+        // Place le premier point du quatrieme quart de l'image.
+        RGBtmp = anamorphose.getRGB(quart4, 0);
+        tableau3D.setPixel(centre + rayon, 0, 0, RGBtmp);
+        // Trace la quatrieme ligne droite.
+        for (int xTmp = (centre + rayon); xTmp < ((centre + rayon) + hauteur); xTmp ++) {
+            int tmp = 1; // Variable necessaire pour la couleur d'une colonne.
+            RGBtmp = anamorphose.getRGB(quart4, tmp++);
+            tableau3D.setPixel(xTmp, 0, 0, RGBtmp);
+        }
+        x4Pre = ((centre + rayon) + hauteur);
+        y4Pre = 0;
+        quart4++;
+        
+        // On dessine ensuite le reste des onctants.
+        while (z > x) {
+            if (d < 0) {
+                d += 2 * x + 3;
+            }
+            else {
+                d += 2 * (x - z) + 5;
+                z --;
+            }
+            x ++;
+            
+            // Place le pixel du premier quart.
+            tableau3D.setPixel((centre - x), 0, z, RGBtmp);
+            int nouveauX = nouveauX (angle, x1Pre, y1Pre, centre, 0);
+            int nouveauY = nouveauY (angle, x1Pre, y1Pre, centre, 0);
+            a = (double) ((nouveauY - z) / (nouveauX - (centre - x)));
+            b = z - (a * (centre - x));
+            for (int xTmp = (centre - x); xTmp < nouveauX; x --) {
+                int tmp = 1;
+                int yTmp = (int) ((a * xTmp) + b);
+                RGBtmp = 200;
+                tableau3D.setPixel(xTmp, 0, yTmp, RGBtmp);
+            }
+            x1Pre = nouveauX;
+            y1Pre = nouveauY;
+            
+            // Place le pixel du deuxieme quart.
+            tableau3D.setPixel((centre - z), 0, x, RGBtmp);
+            
+            // Place le pixel du troisieme quart.
+            tableau3D.setPixel((centre + x), 0, z, RGBtmp);
+            nouveauX = nouveauX (angle, x3Pre, y3Pre, centre, 0);
+            nouveauY = nouveauY (angle, x3Pre, y3Pre, centre, 0);
+            //a = (double) ((nouveauY - z) / (nouveauX - (centre + x)));
+            b = z - (a * (centre + x));
+            for (int xTmp = x; xTmp < nouveauX; xTmp++) {
+                int yTmp = (int) ((a * xTmp) + b);
+                tableau3D.setPixel(xTmp, 0, yTmp, 200);
+            }
+            x3Pre = nouveauX;
+            y3Pre = nouveauY;
+            
+            // Place le pixel du quatrieme quart.
+            tableau3D.setPixel((centre + z), 0, x, RGBtmp);
+            
+        }
+        
         // Remplie la nouvelle image avec celle transforee.
-        for (int z = 0; z < hauteurArrivee; z ++) {
-            for (int x = 0; x < largeurArrivee; x ++) {
-                RGBtmp = tableau3D.getPixel(x, 0, z);
-                imgArrive.setRGB(x, z, RGBtmp);
+        for (int zt = 0; zt < hauteurArrivee; zt ++) {
+            for (int xt = 0; xt < largeurArrivee; xt ++) {
+                RGBtmp = tableau3D.getPixel(xt, 0, zt);
+                imgArrive.setRGB(xt, zt, RGBtmp);
             }
         }
         
