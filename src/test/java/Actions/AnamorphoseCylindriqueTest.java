f4f22b6e59d33e390459ce70b3c9e9ddf3634191
Probak <Probak@192.168.1.18>
2016-07-27 15:40:38 +0200

    Ajout d'une fonction de test d'anamorphosecylindrique

---
+++ src/test/java/Actions/AnamorphoseCylindriqueTest.java
@@ -0,0 +1,27 @@
+package Actions;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+
+import org.junit.Test;
+
+public class AnamorphoseCylindriqueTest 
+{
+
+	// Dimensions de l'image
+	private final int HAUTEUR_IMAGE = 200;
+	private final int LARGEUR_IMAGE = 200;
+	
+	private final int COULEUR_IMAGE = 169;
+	
+	private BufferedImage imageTest = new BufferedImage (LARGEUR_IMAGE, HAUTEUR_IMAGE, TYPE_INT_RGB);
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
